1a68f21f84bcd04e73d71630fd5f83b9324151cd
Adilson Schmitt Junior <adilsonschj@gmail.com>
2019-08-18 16:14:46 +0200

    Add character trought the api

--- DigitalTable.Web/ClientApp/src/features/characterModal/characterModal.tsx
+++ DigitalTable.Web/ClientApp/src/features/characterModal/characterModal.tsx
@@ -1,18 +1,40 @@
 import * as React from 'react';
 import { CharacterList } from 'src/features/characterList/characterList';
+import { useStoreActions } from 'src/store/store';
+import { EntityType } from 'src/domain/entities/entityType';
 
 export function CharacterModal() {
 
 	const [name, setName] = React.useState('');
 	const [description, setDescription] = React.useState('');
+	const createEntity = useStoreActions((actions) => actions.entities.createEntity);
+
+	function createCharacter() {
+		createEntity({
+			name,
+			description,
+			properties: {
+				attributes: {},
+				indexes: {}
+			},
+			type: EntityType.character
+		});
+	}
 
 	return (
 		<div>
 			<CharacterList/>
 			<hr/>
-			<label>Name:</label>
-			<input type="text"/>
-			<button>Add new character</button>
+
+			<label>Name:
+			<input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+			</label>
+
+			<label>description:
+			<input type="text" value={description} onChange={(e) => setDescription(e.target.value)}/>
+			</label>
+
+			<button onClick={createCharacter}>Add new character</button>
 		</div>
 	);
 }
